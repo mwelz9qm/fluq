e7ab5cd26f02bca32c33c70f04f20770dcf6c717
Dayton Singer <daytonsinger02@gmail.com>
2026-08-12 09:12:46 -0600

    add RunConfigBuilder to assist user in constructing run_studies settings.

--- bias_variance/config.py
+++ bias_variance/config.py
@@ -1,22 +1,23 @@
+from collections.abc import Iterable, Mapping
 from dataclasses import dataclass
 from enum import StrEnum
+from typing import Any, Self
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-from bias_variance.generators.base import GeneratorConfig
+from bias_variance.generators.base import VariationGenerator, VariationGeneratorConfig
 from bias_variance.generators.fnn_architecture import (
-    DEFAULT_RANDOM_CONFIG,
-    DEFAULT_TAPER_CONFIG,
+    FnnArchitectureGenerator,
     FnnArchitectureGeneratorConfig,
 )
-from bias_variance.generators.noise import NoiseGeneratorConfig
-from bias_variance.generators.sampling import SamplingGeneratorConfig
+from bias_variance.generators.noise import NoiseGenerator, NoiseGeneratorConfig
+from bias_variance.generators.sampling import SamplingGenerator, SamplingGeneratorConfig
 from bias_variance.models.evaluation import EvaluationMethod, MetricName
 from bias_variance.models.fnn import FnnArchitecture
 
 
-class StudyName(StrEnum):
+class StudyBias(StrEnum):
     MODEL = 'model'
     SAMPLING = 'sampling'
     DATA = 'data'
@@ -24,162 +25,248 @@ class StudyName(StrEnum):
 
 @dataclass(frozen=True, slots=True)
 class Study:
-    study_name: StudyName
-    evaluation_method: EvaluationMethod
-    generator_config: GeneratorConfig
-
-    def __post_init__(self) -> None:
-        match self.study_name:
-            case StudyName.MODEL:
-                if not isinstance(self.generator_config, FnnArchitectureGeneratorConfig):
-                    raise TypeError(
-                        f'Mismatched generator_config type with study_name: {self.generator_config!r}, {self.study_name!r}.'
-                    )
-                
-            case StudyName.SAMPLING:
-                if not isinstance(self.generator_config, SamplingGeneratorConfig):
-                    raise TypeError(
-                        f'Mismatched generator_config type with study_name: {self.generator_config!r}, {self.study_name!r}.'
-                    )
-
-            case StudyName.DATA:
-                if not isinstance(self.generator_config, NoiseGeneratorConfig):
-                    raise TypeError(
-                        f'Mismatched generator_config type with study_name: {self.generator_config!r}, {self.study_name!r}.'
-                    )
-
-    @property
-    def description(self) -> tuple[StudyName, EvaluationMethod]:
-        return (self.study_name, self.evaluation_method)
-
-
-@dataclass(frozen=True, slots=True)
-class DatasetSplit:
-    x_train: pd.DataFrame
-    x_test: pd.DataFrame
-    y_train: pd.DataFrame
-    y_test: pd.DataFrame
-
-    @property
-    def full(
-        self,
-    ) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-        return (self.x_train, self.x_test, self.y_train, self.y_test)
-
-    @property
-    def train_set(
-        self,
-    ) -> pd.DataFrame:
-        return pd.concat([self.x_train, self.y_train], axis='columns')
+    study_bias: StudyBias
+    variation_generator: VariationGenerator
 
 
 @dataclass(frozen=True, slots=True)
 class RunBaseline:
-    inputs: pd.DataFrame
-    outputs: pd.DataFrame
-    split: DatasetSplit
     architecture: FnnArchitecture
+    X: pd.DataFrame
+    Y: pd.DataFrame
+    X_train: pd.DataFrame
+    X_test: pd.DataFrame
+    Y_train: pd.DataFrame
+    Y_test: pd.DataFrame
+
+    @property
+    def split(
+        self,
+    ) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+        return (self.X_train, self.X_test, self.Y_train, self.Y_test)
+    
+    @property
+    def train_set(
+        self,
+    ) -> pd.DataFrame:
+        return pd.concat([self.X_train, self.Y_train], axis='columns')
 
     @property
     def dataset(
         self,
     ) -> pd.DataFrame:
-        return pd.concat([self.inputs, self.outputs], axis='columns')
+        return pd.concat([self.X, self.Y], axis='columns')
 
 
 @dataclass(frozen=True, slots=True)
 class RunConfig:
     baseline: RunBaseline
     studies: tuple[Study, ...]
+    evaluation_methods: tuple[EvaluationMethod, ...]
     n_iter: int
     test_size: float
     test_metrics: frozenset[MetricName]
     random_state: int | None
 
 
-def _build_run_config(
-    inputs: pd.DataFrame,
-    outputs: pd.DataFrame,
-    studies: tuple[Study, ...] | None = None,
-    base_architecture: tuple[int, ...] = (64, 64, 64, 64, 64, 64, 64, 64),
-    n_iter: int = 100,
-    test_size: float = 0.2,
-    test_metrics: frozenset[MetricName] = frozenset(
-        (MetricName.MSE, MetricName.R2)
-    ),
-    random_state: int | None = None
-) -> RunConfig:
-    split = train_test_split(
-        inputs,
-        outputs,
-        test_size=test_size,
-        random_state=random_state
-    )
+class RunConfigBuilder:
+    def __init__(self):
+        self._config_data: dict[str, Any] = {}
+        self.reset()
 
-    baseline = RunBaseline(
-        inputs=inputs,
-        outputs=outputs,
-        split=DatasetSplit(*split),
-        architecture=FnnArchitecture(base_architecture)
-    )
+    def reset(self) -> None:
+        self._config_data = {
+            'base_architecture': FnnArchitecture((64, 64, 64, 64, 64, 64, 64, 64)),
+            'evaluation_methods': [
+                EvaluationMethod.AVERAGING,
+                EvaluationMethod.POINTWISE
+            ],
+            'n_iter': 100,
+            'test_size': 0.2,
+            'test_metrics': [MetricName.MSE, MetricName.R2],
+        }
 
-    if studies is None:
-        default_studies: list[Study] = []
-        default_studies.append(
-            Study(
-                StudyName.MODEL,
-                EvaluationMethod.AVERAGING,
-                FnnArchitectureGeneratorConfig(
-                    range_architectures=DEFAULT_RANDOM_CONFIG,
-                    taper_architectures=DEFAULT_TAPER_CONFIG
-                )
-            )
-        )
-        default_studies.append(
-            Study(
-                StudyName.SAMPLING,
-                EvaluationMethod.AVERAGING,
-                SamplingGeneratorConfig(baseline.dataset)
-            )
-        )
-        default_studies.append(
-            Study(
-                StudyName.DATA,
-                EvaluationMethod.AVERAGING,
-                NoiseGeneratorConfig(baseline.dataset)
-            )
-        )
-        default_studies.append(
-            Study(
-                StudyName.MODEL,
-                EvaluationMethod.POINTWISE,
-                FnnArchitectureGeneratorConfig(
-                    range_architectures=DEFAULT_RANDOM_CONFIG,
-                    taper_architectures=DEFAULT_TAPER_CONFIG
-                )
-            )
-        )
-        default_studies.append(
-            Study(
-                StudyName.SAMPLING,
-                EvaluationMethod.POINTWISE,
-                SamplingGeneratorConfig(baseline.split.train_set)
-            )
-        )
-        default_studies.append(
-            Study(
-                StudyName.DATA,
-                EvaluationMethod.POINTWISE,
-                NoiseGeneratorConfig(baseline.split.train_set)
-            )
-        )
-        studies = tuple(default_studies)
+    def _set(self, key: str, value: Any) -> Self:
+        self._config_data[key] = value
+        return self
 
-    return RunConfig(
-        baseline=baseline,
-        studies=studies,
-        n_iter=n_iter,
-        test_size=test_size,
-        test_metrics=test_metrics,
-        random_state=random_state
-    )
+    def set_X(self, X: pd.DataFrame) -> Self:
+        return self._set('X', X)
+
+    def set_Y(self, Y: pd.DataFrame) -> Self:
+        return self._set('Y', Y)
+
+    def set_variation_generator_configs(
+        self,
+        variation_generator_configs: Iterable[VariationGeneratorConfig] | Mapping[str, Mapping[str, Any]]
+    ) -> Self:
+        if variation_generator_configs is None:
+            raise ValueError(
+                'Empty variation_generator_configs not allowed.'
+                'Must contain at least one variation generator config.'
+            )
+
+        if isinstance(variation_generator_configs, Iterable[VariationGeneratorConfig]):
+            return self._set('variation_generator_configs', variation_generator_configs)
+
+        elif isinstance(variation_generator_configs, Mapping[str, Mapping[str, Any]]):
+            converted_configs: list[VariationGeneratorConfig] = []
+            for key, kwargs in variation_generator_configs.items():
+                match key:
+                    case StudyBias.MODEL.value:
+                        converted_configs.append(FnnArchitectureGeneratorConfig(**kwargs))
+
+                    case StudyBias.SAMPLING.value:
+                        converted_configs.append(SamplingGeneratorConfig(**kwargs))
+
+                    case StudyBias.DATA.value:
+                        converted_configs.append(NoiseGeneratorConfig(**kwargs))
+
+            return self._set('variation_generator_configs', converted_configs)
+
+        else:
+            raise TypeError(
+                'Unknown variation generator type.'
+            )
+        
+    def set_evaluation_methods(
+        self,
+        evaluation_methods: Iterable[EvaluationMethod] | Iterable[str]
+    ) -> Self:
+        if evaluation_methods is None:
+            raise ValueError(
+                'Empty evaluation_methods not allowed.'
+                'Must contain at least one evaluation method.'
+            )
+        
+        if isinstance(evaluation_methods, Iterable[EvaluationMethod]):
+            return self._set('evaluation_methods', evaluation_methods)
+
+        converted_methods: list[EvaluationMethod] = []
+        for method in evaluation_methods:
+            match method:
+                case EvaluationMethod.AVERAGING.value:
+                    converted_methods.append(EvaluationMethod.AVERAGING)
+
+                case EvaluationMethod.POINTWISE.value:
+                    converted_methods.append(EvaluationMethod.POINTWISE)
+
+                case _:
+                    raise ValueError(
+                        f'Unknown evaluation method: {method}'
+                    )
+        return converted_methods
+
+    def set_base_architecture(self, base_architecture: FnnArchitecture | tuple[int, ...]) -> Self:
+        if isinstance(base_architecture, tuple[int, ...]):
+            return self._set('base_architecture', FnnArchitecture(base_architecture))
+
+        elif isinstance(base_architecture, FnnArchitecture):
+            return self._set('base_architecture', base_architecture)
+
+        else:
+            raise TypeError(
+                'Unknown base_architecture type.'
+            )
+
+    def set_n_iter(self, n_iter: int) -> Self:
+        return self._set('n_iter', n_iter)
+
+    def set_test_size(self, test_size: float) -> Self:
+        return self._set('test_size', test_size)
+
+    def set_test_metrics(self, test_metrics: Iterable[MetricName] | Iterable[str]) -> Self:
+        if isinstance(test_metrics, Iterable[MetricName]):
+            return self._set('test_metrics', test_metrics)
+
+        elif isinstance(test_metrics, Iterable[str]):
+            converted_metrics: list[MetricName] = []
+            for metric in test_metrics:
+                match metric:
+                    case MetricName.MSE.value:
+                        converted_metrics.append(MetricName.MSE)
+
+                    case MetricName.RMSE.value:
+                        converted_metrics.append(MetricName.RMSE)
+
+                    case MetricName.MAE.value:
+                        converted_metrics.append(MetricName.MAE)
+
+                    case MetricName.R2.value:
+                        converted_metrics.append(MetricName.R2)
+
+                    case _:
+                        raise ValueError(
+                            f'Unsupported test metric: {metric!r}.'
+                        )
+
+            return self._set('test_metrics', converted_metrics)
+
+        else:
+            raise TypeError(
+                'Unknown test_metrics type.'
+            )
+
+    def set_random_state(self, random_state: int) -> Self:
+        return self._set('random_state', random_state)
+
+    def build(self) -> RunConfig:
+        X = self._config_data.get('X', None)
+        Y = self._config_data.get('Y', None)
+        test_size = self._config_data.get('test_size', None)
+        random_state = self._config_data.get('random_state', None)
+
+        split = train_test_split(
+            X,
+            Y,
+            test_size=test_size,
+            random_state=random_state
+        )
+
+        base_architecture = self._config_data.get('base_architecture', None)
+        
+        baseline = RunBaseline(
+            base_architecture,
+            X,
+            Y,
+            *split,        
+        )
+
+        variation_generator_configs = self._config_data.get('variation_generator_configs', None)
+
+        studies = []
+        
+        if variation_generator_configs is None:
+            default_studies = [
+                Study(StudyBias.MODEL, FnnArchitectureGenerator()),
+                Study(StudyBias.SAMPLING, SamplingGenerator()),
+                Study(StudyBias.DATA, NoiseGenerator())
+            ]
+            studies = default_studies
+        else:
+            for config in variation_generator_configs:
+                match config:
+                    case FnnArchitectureGeneratorConfig():
+                        studies.append(Study(StudyBias.MODEL, FnnArchitectureGenerator(config)))
+        
+                    case SamplingGeneratorConfig():
+                        studies.append(Study(StudyBias.SAMPLING, SamplingGenerator(config)))
+        
+                    case NoiseGeneratorConfig():
+                        studies.append(Study(StudyBias.DATA, NoiseGenerator(config)))
+
+        evaluation_methods = self._config_data.get('evaluation_methods', None)
+        n_iter = self._config_data.get('n_iter', None)
+        test_metrics = self._config_data.get('test_metrics', None)
+
+        self.reset()
+
+        return RunConfig(
+            baseline=baseline,
+            studies=tuple(studies),
+            evaluation_methods=evaluation_methods,
+            n_iter=n_iter,
+            test_size=test_size,
+            test_metrics=test_metrics,
+            random_state=random_state
+        )
